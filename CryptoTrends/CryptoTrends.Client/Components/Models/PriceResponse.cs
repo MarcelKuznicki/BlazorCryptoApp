86fb13e9000fc523e51829b622b1a432f0a9d72e
marcelGit <marcel.kuznicki0121@gmail.com>
2025-12-17 10:37:47 +0100

    Initial commit - Blazor app

---
+++ CryptoTrends/CryptoTrends.Client/Components/Models/PriceResponse.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace CryptoTrends.Client.Components.Models
+{
+    public class CryptoMarket
+    {
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        [JsonProperty("current_price")] public decimal CurrentPrice { get; set; }
+        [JsonProperty("market_cap")] public decimal MarketCap { get; set; }
+        [JsonProperty("price_change_percentage_24h")] public decimal PriceChange24h { get; set; }
+    }
+}
---
+++ CryptoTrends/CryptoTrends/Program.cs
@@ -0,0 +1,48 @@
+﻿using Blazored.LocalStorage;
+using CryptoTrends.Client;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddBlazoredLocalStorage();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode();
+
+var config = builder.Configuration;
+
+var coinGeckoBaseUrl = config["CoingeckoApi:BaseUrl"];
+var coinGeckoApiKey = config["CoingeckoApi:ApiKey"];
+
+//builder.Services.AddHttpClient("CoinGecko", client =>
+//{
+//    client.BaseAddress = new Uri(coinGeckoBaseUrl);
+//    client.DefaultRequestHeaders.Add("accept", "application/json");
+//    client.DefaultRequestHeaders.Add("x-cg-demo-api-key", coinGeckoApiKey);
+//});
+
+app.Run();
